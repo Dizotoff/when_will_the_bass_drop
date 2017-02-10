5a054f743241c0ebeed8ac3b6e56f87ff04a7ad0
Dmytro Izotov <dmytroi@metropolia.fi>
2017-02-10 12:43:20 +0200

    WASD controller and camerafollowing are working

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour {
-
+	GameObject player;
+	bool followPlayer = true;
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (followPlayer == true) {
+			camFollowPlayer ();
+		}
 	}
+		public void setFollowPlayer (bool val)
+		{
+			followPlayer = val;
+		}
+
+		void camFollowPlayer()
+	
+		{
+		Vector3 newPos = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z);
+		this.transform.position = newPos;
+
+		}
+
+
+
+
 }
