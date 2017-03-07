bfb1febf1a4807d89f4f5dd847a979f393178085
dimas <izotov.dima@gmail.com>
2017-03-07 20:27:34 +0200

    score added

--- Assets/Scripts/EnemyAttacked.cs
+++ Assets/Scripts/EnemyAttacked.cs
@@ -8,12 +8,13 @@ public class EnemyAttacked : MonoBehaviour {
 	bool EnemyKnockedDown=false;
 	float knockDownTimer = 3.0f;
 	GameObject player;
+	ScoreController sc;
 
 
 	void Start () {
 		sr = this.GetComponent<SpriteRenderer> ();
 		player = GameObject.FindGameObjectWithTag ("Player");
-
+		sc = GameObject.FindGameObjectWithTag ("GameController").GetComponent<ScoreController> ();
 	}
 
 
@@ -33,13 +34,14 @@ public class EnemyAttacked : MonoBehaviour {
 	{
 
 
+
 		knockDownTimer -= Time.deltaTime;
 		sr.sprite = knockedDown;
 		this.GetComponent<CircleCollider2D> ().enabled = false;
 		this.GetComponent<EnemyAI> ().enabled = false;
 
-
 		if (knockDownTimer <= 0) {
+			sc.AddScore (500,this.transform.position);
 			EnemyKnockedDown = false;
 			sr.sprite = backUp;
 			this.GetComponent<EnemyAI> ().enabled = true;
@@ -52,7 +54,7 @@ public class EnemyAttacked : MonoBehaviour {
 
 	public void killBullet()
 	{
-
+		sc.AddScore (500,this.transform.position);
 		sr.sprite = bulletWound;
 		Instantiate(bloodPool, this.transform.position, this.transform.rotation);
 		//disable ai
@@ -64,7 +66,7 @@ public class EnemyAttacked : MonoBehaviour {
 
 	public void killMelee()
 	{
-
+		sc.AddScore (1000,this.transform.position);
 		sr.sprite = stabbed;
 		Instantiate (bloodPool,this.transform.position,this.transform.rotation);
 		Instantiate (bloodSpurt,this.transform.position,player.transform.rotation);
