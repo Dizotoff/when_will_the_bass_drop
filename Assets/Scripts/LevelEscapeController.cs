101d20ff2893b2f901b7344f6d6e01dd622f9fe4
Emily Hernandez <bulle.96@windowslive.com>
2017-03-12 19:27:25 +0200

    pulled changes from remote repo, configured the .gitignore for the project

---
+++ Assets/Scripts/LevelEscapeController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LevelEscapeController : MonoBehaviour {
+	public GameObject col,van;
+	public Sprite doorsOpen;
+	SpriteRenderer sr;
+	 GameObject[] enemies;
+	// Use this for initialization
+	void Start () {
+		sr = this.GetComponent<SpriteRenderer> ();
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+
+
+	}
+
+	void Update () {
+		if (areAllEnemiesDead () == true && sr.sprite != doorsOpen) {
+			openCar ();
+
+		}
+
+
+	}
+
+
+	public bool areAllEnemiesDead()
+	{
+		for (int x = 0; x < enemies.Length; x++) {
+			if (enemies [x].tag != "Dead") {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	void openCar()
+	{
+		Destroy (col);
+		sr.sprite = doorsOpen;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+		 
+			Debug.Log ("COOOOOLLLLIIIDDEEEERRR!!!!");
+
+			MenuScreen menu = GameObject.FindGameObjectWithTag ("Menu").GetComponent<MenuScreen> ();
+
+			menu.saveHighScore ();
+			menu.display = true;
+			SceneManager.LoadScene ("Menu");
+		}
+
+
+
+	}
+}
---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour {
+	public bool playing = false;
+	// Use this for initialization
+	public MovieTexture movie;
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		movie.loop= true;
+		movie.Play ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		playing = true;
+
+	}
+}
