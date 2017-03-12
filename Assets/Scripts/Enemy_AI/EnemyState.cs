30f6603fd724f755bc7bae8810da814304b0c8bb
Emily Hernandez <bulle.96@windowslive.com>
2017-03-12 17:16:36 +0200

    divided enemy states in separate classes, lots of errors because I'm nowhere done yet

---
+++ Assets/Scripts/Enemy_AI/EnemyState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour {
+
+	[HideInInspector]
+	public IEnemyState currentState;
+
+	[HideInInspector]
+	public PatrolState patrolState;
+
+	[HideInInspector]
+	public AlertState alertState;
+
+	[HideInInspector]
+	public ChaseState chaseState;
+
+	void Awake (){
+		patrolState = new PatrolState(this);
+		alertState = new AlertState(this);
+		chaseState = new ChaseState(this);
+	}
+
+
+	void Start() {
+		currentState = patrolState;
+	}
+
+	void Update() {
+		currentState.UpdateState ();
+	}
+
+	private void onTriggerEnter(Collider other){
+		currentState.OnTriggerEnter ();
+	}
+
+}
---
+++ Assets/Scripts/Enemy_AI/IEnemyState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemyState {
+	void UpdateState();
+
+	void OnTriggerEnter(Collider other);
+
+	void ToPatrolState();
+
+	void ToAlertState();
+
+	void ToChaseState();
+}
