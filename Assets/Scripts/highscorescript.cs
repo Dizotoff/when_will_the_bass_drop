2bc628d1791a97ac46ca817342cc13a744b4f9b2
Sarita khanal <saritakh@metropolia.fi>
2017-03-14 11:01:13 +0000

    added highscore scripts etc

---
+++ Assets/Scripts/highscorescript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highscorescript :MonoBehaviour  {
+	
+	public GameObject Score;
+	public GameObject ScoreName;
+	public GameObject Rank;
+
+	public  void SetScore(string name,string score,string rank) // all the things are string because we are going to insert everything on text.
+	{
+		this.Rank.GetComponent<Text>().text = rank;
+		this.ScoreName.GetComponent<Text>().text = name;
+		this.Score.GetComponent<Text>().text = score;
+	}
+}
